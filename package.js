175436698d0a60fb787bf12e1db0285979a77826
Bevan Hunt <me@bevanhunt.com>
2012-10-29 07:21:06 -0700

    fix validation section in package

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ Package.describe({
 Package.on_use(function (api, where) {
   api.add_files('lib/knockout/knockout.js', 'client');
   api.add_files('lib/knockout/knockout.mapping.js', 'client');
-  api.add_files('lib/knockout-validation/Dist/knockout-validation.js');
-  api.add_files('lib/knockout-validation/Localization/en-US.js');
+  api.add_files('lib/knockout.validation/knockout.validation.js', 'client');
+  api.add_files('lib/knockout.validation/en-US.js', 'client');
   api.add_files('lib/knockout.meteor/build/knockout.meteor.js', 'client');
 });
